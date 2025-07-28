a2e7c3668fc333c48382bdde73d65b7f87a71ff9
ams-mikhail <alphaglasson@mail.ru>
2025-07-28 17:11:36 +0700

    update: реализован запуск сервера

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,28 +1,58 @@
 package main
 
 import (
+	"context"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+	"wallet_api/internal/config"
+	"wallet_api/internal/database"
+	"wallet_api/internal/errors"
 	"wallet_api/internal/handler"
+	"wallet_api/internal/repository"
 	"wallet_api/internal/server"
+	"wallet_api/internal/service"
 	"github.com/spf13/viper"
 )
 
 func main() {
-	InitConfig()
-	service := 0
-	handler := handler.NewHandler(service)
+	config := config.InitConfig()
+
+	db, err := database.ConnectPostgres()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer db.Close()
+	err = database.RunMigrations(db)
+	if err != nil {
+		log.Fatal(errors.ErrMigrations)
+	}
+	err = database.DBSeedWallets(db)
+	if err != nil {
+		log.Fatal(errors.ErrSeed)
+	}
+
+	repository := repository.NewRepository(db, config)
+	service := service.NewService(repository, config)
+	handler := handler.NewHandler(service, config)
 	server := &server.Server{}
 
-	if err := server.Run(viper.GetString("port"), handler.InitRoutes()); err != nil {
-		log.Fatal("server error")
-	}
-}
-
-func InitConfig() {
-	viper.AddConfigPath("configs")
-	viper.SetConfigName("config")
-	err := viper.ReadInConfig()
-	if err != nil {
-		log.Fatalf("%s", err.Error())
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	// запуск сервера
+	go func() {
+		if err := server.Run(viper.GetString("port"), handler.InitRoutes()); err != nil {
+			log.Fatal(errors.ErrServerRun)
+		}
+	}()
+	// выключение сервера
+	<-quit
+	log.Println("shutting down")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal("shutdown failed: ", err.Error())
 	}
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,11 @@
 // Package config реализация работы с настройками проекта
 package config
 
-import "github.com/spf13/viper"
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
 
 type Config struct {
 	MaxRowsLimit int
@@ -11,4 +15,15 @@ func NewConfig() *Config {
 	return &Config{
 		MaxRowsLimit: viper.GetInt("db.max_rows_query_limit"),
 	}
+}
+
+func InitConfig() *Config {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("%s", err.Error())
+	}
+
+	return NewConfig()
 }
